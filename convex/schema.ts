4ab5667cb6d02d174e9fe8d50cab90ed3f8ceb45
Harsh Mriduhash <Harshsahay2709@gmail.com>
2025-01-28 11:58:15 +0530

    writing schema

--- convex/schema.ts
+++ convex/schema.ts
@@ -2,16 +2,16 @@ import { defineSchema, defineTable } from "convex/server";
 import { v } from "convex/values";
 
 export default defineSchema({
-    chats: defineTable({
-        title: v.string(),
-        userId: v.string(),
-        createdAt: v.number(),
-    }).index("by_user_id", ["userId"]), // optimized query for chats by user
+  chats: defineTable({
+    title: v.string(),
+    userId: v.string(),
+    createdAt: v.number(),
+  }).index("by_user_id", ["userId"]), // optimized query for chats by user
 
-    messages: defineTable({
-        chatId: v.id("chats"), // reference to chat id table (relation)
-        content: v.string(),
-        role: v.union(v.literal("user"), v.literal("assistant")),
-        createdAt: v.number(),
-    }).index("by_chat_id", ["chatId"]),
+  messages: defineTable({
+    chatId: v.id("chats"), // reference to chat id table (relation)
+    content: v.string(),
+    role: v.union(v.literal("user"), v.literal("assistant")),
+    createdAt: v.number(),
+  }).index("by_chat_id", ["chatId"]),
 });
