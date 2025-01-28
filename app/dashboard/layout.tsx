db3e59a709e158379f4b37ee0cedc2c1563aa83c
Harsh Mriduhash <Harshsahay2709@gmail.com>
2025-01-28 12:58:04 +0530

    building dashboard

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -6,23 +6,23 @@ import { NavigationProvider } from "@/lib/navigationProvider";
 import { Authenticated } from "convex/react";
 
 export default function DashboardLayout({
-    children,
+  children,
 }: {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }) {
-    return (
-        <NavigationProvider>
-            <div className="flex h-screen">
-                <Authenticated>
-                    <Sidebar />
-                </Authenticated>
+  return (
+    <NavigationProvider>
+      <div className="flex h-screen">
+        <Authenticated>
+          <Sidebar />
+        </Authenticated>
 
-                <div className="flex-1">
-                    <Header />
+        <div className="flex-1">
+          <Header />
 
-                    <main>{children}</main>
-                </div>
-            </div>
-        </NavigationProvider>
-    );
-};
+          <main>{children}</main>
+        </div>
+      </div>
+    </NavigationProvider>
+  );
+}
